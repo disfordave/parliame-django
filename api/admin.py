bc902b4215acdaed90ea82af19b7c07064f7d743
Dave <118544840+disfordave@users.noreply.github.com>
2026-01-20 21:05:07 +0900

    Refactor admin registration with custom ModelAdmin classes, showing search fields and filters

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
 from .models import Country, Chamber, Party, Poll, PollResult
 
-admin.site.register(Country)
-admin.site.register(Chamber)
-admin.site.register(Party)
-admin.site.register(Poll)
-admin.site.register(PollResult)
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "emoji", "created_at")
+    search_fields = ("name", "code")
+
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ("short_name", "name", "country", "position", "is_independent")
+    list_filter = ("country", "is_independent")
+    search_fields = ("name", "short_name")
+
+
+@admin.register(Chamber)
+class ChamberAdmin(admin.ModelAdmin):
+    list_display = ("name", "short_name", "country", "total_seats")
+    list_filter = ("country",)
+    search_fields = ("name", "short_name")
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ("slug", "chamber", "created_at", "date")
+    list_filter = ("chamber", "date")
+    search_fields = ("slug",)
+
+
+@admin.register(PollResult)
+class PollResultAdmin(admin.ModelAdmin):
+    list_display = ("poll", "party", "seats", "vote_share")
+    list_filter = ("poll", "party")
