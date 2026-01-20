93b4499288c26288e917105553d76bf0aa0c14d8
Seokwoo Hong <118544840+disfordave@users.noreply.github.com>
2026-01-20 11:42:23 +0900

    Add viewsets for Country, Chamber, Party, and Poll models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Country, Chamber, Party, Poll
+from .serializers import (
+    CountrySerializer,
+    ChamberSerializer,
+    PartySerializer,
+    PollSerializer,
+)
 
-# Create your views here.
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    lookup_field = "code"
+
+
+class ChamberViewSet(viewsets.ModelViewSet):
+    queryset = Chamber.objects.all()
+    serializer_class = ChamberSerializer
+    lookup_field = "slug"
+
+
+class PartyViewSet(viewsets.ModelViewSet):
+    queryset = Party.objects.all()
+    serializer_class = PartySerializer
+    lookup_field = "slug"
+
+
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    lookup_field = "slug"
